8af97e24a0a4db6c6534017bbc844b394d871502
Anton <antonoliverkiiski@gmail.com>
2023-02-13 12:52:49 +0200

    Initial commit for signuppage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule

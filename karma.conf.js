ddfd1df63ac3a77ff692875d16b8e452c888fbd2
Miconen <micropizza.mc@gmail.com>
2023-02-14 16:33:02 +0200

    Added karma-junit-reporter to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,15 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-junit-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       jasmine: {
@@ -19,29 +20,29 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      dir: require("path").join(__dirname, "./coverage/frontend"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    junitReporter: {
+      outputDir: "testresults",
     },
     autoWatch: false,
     singleRun: true,
-    reporters: ['progress', 'kjhtml'],
-    browsers: ['HeadlessChrome'],
+    restartOnFileChange: true,
+    reporters: ["progress", "kjhtml", "junit"],
+    browsers: ["HeadlessChrome"],
     customLaunchers: {
       HeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
     },
-    restartOnFileChange: true
   });
 };

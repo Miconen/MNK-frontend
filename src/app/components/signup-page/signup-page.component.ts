aab656dcd3854a967b156326c731195f071e0051
Niko <soder.n@gmail.com>
2023-02-15 13:14:17 +0200

    Add login form validation

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -8,13 +8,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupPageComponent {
   signupForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
     primaryEmail: new FormControl('', [
       Validators.required,
       Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
     ]),
-    password: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      Validators.maxLength(20),
+    ]),
   });
 
   onCreate() {

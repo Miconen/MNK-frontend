8ed640649d3743dca3e28bbad3491effc2733998
Niko <soder.n@gmail.com>
2023-02-28 12:31:57 +0200

    Add onSaveChanges method

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  saveChangesForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  onSaveChanges() {
+    console.log(this.saveChangesForm.value);
+    // TODO:
+    // if new username or new password is valid then
+    // do PUT request to api and update old values with new values
+    // in database.
+  }
 }

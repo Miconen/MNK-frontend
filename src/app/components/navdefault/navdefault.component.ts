5c208444a90cde2e1d5c887eb59b134d25a0bdf5
Anton <antonoliverkiiski@gmail.com>
2023-03-13 14:02:03 +0200

    Add toggle button for darkmode and make it keep the selection in local storage

--- src/app/components/navdefault/navdefault.component.ts
+++ src/app/components/navdefault/navdefault.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
+import { faSun } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-navdefault',
   templateUrl: './navdefault.component.html',
-  styleUrls: ['./navdefault.component.scss']
+  styleUrls: ['./navdefault.component.scss'],
 })
 export class NavdefaultComponent {
+  faMoon = faMoon;
+  faSun = faSun;
 
+  public isDarkMode = false;
+
+  ngOnInit() {
+    // get the saved state of the checkbox from local storage
+    const checkboxState = localStorage.getItem('isDarkMode');
+
+    // if the state exists, set the isDarkMode variable to the saved state
+    if (checkboxState !== null) {
+      this.isDarkMode = checkboxState === 'true';
+    } else {
+      // otherwise, set the isDarkMode variable based on system preferences
+      this.isDarkMode = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches;
+      localStorage.setItem('isDarkMode', String(this.isDarkMode));
+    }
+
+    // add or remove the 'dark' class based on the current checkbox state
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+  toggleDarkMode() {
+    // toggle the isDarkMode variable
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('isDarkMode', String(this.isDarkMode));
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  saveCheckboxState() {
+    // save the state of the checkbox to local storage
+    localStorage.setItem('checkboxState', JSON.stringify(this.isDarkMode));
+  }
 }

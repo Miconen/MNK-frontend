6bcb3c2396efee22a7d8b6f5e385a7e2debda878
Miconen <micropizza.mc@gmail.com>
2023-03-15 13:22:33 +0200

    Remove authService from signalrService

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,65 +1,59 @@
 import { Injectable } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
 import { IChatEvent } from 'src/app/types/message.interface';
-import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class SignalrService {
-  public data: any[]; // Change any type
-  public ENDPOINT: string = 'https://localhost:8081/api/hub/chat';
-  private hubConnection: signalR.HubConnection;
+    public data: any[]; // Change any type
+    public ENDPOINT: string = 'https://localhost:8081/api/hub/chat';
+    private hubConnection: signalR.HubConnection;
 
-  constructor(private authService: AuthService) {
-    this.data = [];
-    this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl(this.ENDPOINT)
-      .build();
-  }
-
-  public startConnection = async () => {
-    await this.hubConnection.start();
-    let CONNECTED = this.hubConnection.state === 'Connected';
-    if (!CONNECTED) {
-      console.log('Error occurred with the chat connection');
-      return;
+    constructor(private authService: AuthService) {
+        this.data = [];
+        this.hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl(this.ENDPOINT)
+            .build();
     }
-    console.log('Connection started');
-    return;
-  };
 
-  public addChatListener = async (userId: string) => {
-    console.log('Attaching chat listener');
-    const getYou = (messageUser: string) =>
-      userId === messageUser ? 'You' : messageUser;
-    this.hubConnection.on('ReceiveMessage', (user: string, message: string) => {
-      this.data.push({ user: getYou(user), content: message });
-    });
-  };
+    public startConnection = async () => {
+        await this.hubConnection.start();
+        let CONNECTED = this.hubConnection.state === 'Connected';
+        if (!CONNECTED) {
+            console.log('Error occurred with the chat connection');
+            return;
+        }
+        console.log('Connection started');
+        return;
+    };
 
-  public joinGroup = async (userId: string, groupName: string) => {
-    this.hubConnection.invoke(
-      'JoinGroup',
-      userId,
-      groupName,
-      this.authService.getToken()
-    );
-    this.hubConnection.invoke(
-      'SendMessageToGroup',
-      userId,
-      'Test message',
-      groupName,
-      this.authService.getToken()
-    );
-  };
+    public addChatListener = async (userId: string) => {
+        console.log('Attaching chat listener');
+        const getYou = (messageUser: string) =>
+            userId === messageUser ? 'You' : messageUser;
+        this.hubConnection.on('ReceiveMessage', (user: string, message: string) => {
+            this.data.push({ user: getYou(user), content: message });
+        });
+    };
 
-  public sendMessage = async (chatEvent: IChatEvent, groupName: string) => {
-    this.hubConnection.invoke(
-      'SendMessageToGroup',
-      chatEvent,
-      groupName,
-      this.authService.getToken()
-    );
-  };
+    public joinGroup = async (event: IChatEvent) => {
+        this.hubConnection.invoke(
+            'JoinGroup',
+            event.Username,
+            event.Roomname,
+            event.JWT,
+        );
+        this.hubConnection.invoke(
+            'SendMessageToGroup',
+            event
+        );
+    };
+
+    public sendMessage = async (event: IChatEvent) => {
+        this.hubConnection.invoke(
+            'SendMessageToGroup',
+            event,
+        );
+    };
 }

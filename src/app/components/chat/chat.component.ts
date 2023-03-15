97048791c811163612a01b6d0b99a9c0b9a4b480
Miconen <micropizza.mc@gmail.com>
2023-03-15 13:22:55 +0200

    Fixed indentation and test event

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -5,63 +5,65 @@ import { AuthService } from 'src/app/services/auth.service';
 import { IChatEvent } from 'src/app/types/message.interface';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss'],
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent {
-  constructor(
-    public signalRService: SignalrService,
-    private http: HttpClient,
-    private authService: AuthService
-  ) {}
+    constructor(
+        public signalRService: SignalrService,
+        private http: HttpClient,
+        private authService: AuthService
+    ) { }
 
-  async ngOnInit() {
-    await this.signalRService.startConnection();
-    await this.signalRService.addChatListener(this.authService.getUsername());
-    this.startHttpRequest();
-    await this.signalRService.joinGroup(this.authService.getUsername(), 'test');
-  }
-
-  private startHttpRequest = () => {
-    this.http.get(this.signalRService.ENDPOINT).subscribe((res) => {
-      console.log(res);
-    });
-  };
-
-  public getData = () => {
-    let data = this.signalRService.data;
-    return JSON.stringify(data);
-  };
-
-  get isLoggedIn(): boolean {
-    return this.authService.isLoggedIn();
-  }
-
-  sendMessage(message: HTMLTextAreaElement, chatWindow: HTMLDivElement) {
-    if (message.value.trim() === '') {
-      return;
+    async ngOnInit() {
+        await this.signalRService.startConnection();
+        await this.signalRService.addChatListener(this.authService.getUsername());
+        this.startHttpRequest();
+        await this.signalRService.joinGroup(this.authService.getUsername(), 'test');
     }
 
-    const date = new Date();
-
-    let userMessage: IChatEvent = {
-      Date: date,
-      Content: message.value,
-      ContentType: 'Message',
-      Username: 'testUser',
+    private startHttpRequest = () => {
+        this.http.get(this.signalRService.ENDPOINT).subscribe((res) => {
+            console.log(res);
+        });
     };
 
-    this.signalRService.sendMessage(userMessage, 'test');
+    public getData = () => {
+        let data = this.signalRService.data;
+        return JSON.stringify(data);
+    };
 
-    /* reset textarea value */
-    message.value = '';
-    /* chatwindow stays on bottom when there is more messages coming */
-    chatWindow.scrollTop = chatWindow.scrollHeight;
-  }
+    get isLoggedIn(): boolean {
+        return this.authService.isLoggedIn();
+    }
 
-  // if message user is not You then align text right
-  checkUser(user: string) {
-    return user === 'You' ? '' : 'content-right';
-  }
+    sendMessage(message: HTMLTextAreaElement, chatWindow: HTMLDivElement) {
+        if (message.value.trim() === '') {
+            return;
+        }
+
+        const date = new Date();
+
+        let event: IChatEvent = {
+            Date: date,
+            Content: message.value,
+            ContentType: 'Message',
+            Username: this.authService.getUsername(),
+            Roomname: 'test',
+            JWT: this.authService.getToken(),
+        };
+
+        this.signalRService.sendMessage(event);
+
+        /* reset textarea value */
+        message.value = '';
+        /* chatwindow stays on bottom when there is more messages coming */
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+
+    // if message user is not You then align text right
+    checkUser(user: string) {
+        return user === 'You' ? '' : 'content-right';
+    }
 }
